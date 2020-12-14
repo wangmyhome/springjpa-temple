59f17a0bc486630cfbb5dbd6bb4decf70e215fa4
= <=>
2020-12-14 17:25:42 +0800

    多表查询：1自定义返回模板，2原生sql

--- src/main/java/io/wang/jpa/repository/CustomerRepository.java
+++ src/main/java/io/wang/jpa/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import io.wang.jpa.entity.Customer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -38,4 +39,32 @@ public interface CustomerRepository extends JpaRepository<Customer, String> {
      * @return
      */
     Page<Customer> findAllByCustIdLikeAndCustNameLike(String custId, String custName, Pageable pageable);
+
+
+
+    //多表查询
+//    @Query(nativeQuery = true,value="SELECT\n" +
+//            "\ttom.id,\n" +
+//            "\ttom.device_no,\n" +
+//            "\ttcir.commodity_id as commodity_num,\n" +
+//            "\ttci.COMMODITY_NAME as commodity_name,\n" +
+//            "\ttom.CREATE_TIME,\n" +
+//            "\ttom.status,\n" +
+//            "\ttom.app_from,\n" +
+//            "\ttom.province_id,\n" +
+//            "\tcp.name_tx  as province_name\n" +
+//            "FROM\n" +
+//            "\t T_ORDER_MAIN tom\n" +
+//            "\tLEFT JOIN T_COMMODITY_ORDER tco ON tom.ID = tco.PARENT_ORDER_ID\n" +
+//            "\tLEFT JOIN T_COMMODITY_INORDER tcir ON tco.ORDER_ID = tcir.ORDER_ID\n" +
+//            "\tLEFT JOIN t_commodity_info tci ON tcir.COMMODITY_ID = tci.COMMODITY_ID\n" +
+//            "\tLEFT JOIN CODE_PROVINCE cp ON cp.VALUE_TX = tom.PROVINCE_ID")
+//    List<OrderMainEntity> findAllByOrderMainEntity();
+
+
+
+    //自定义返回对象查询
+//    @Query(value = "SELECT new cn.gogpay.dcb.province.taxation.dto.FilingHistoryDTO(s.name, s.startDate, s.endDate) " +
+//            "FROM FilingFinancialAccountingSystemEntity AS s WHERE s.userId = ?1")
+//    List<FilingHistoryDTO> findAllByUserId(String userId);
 }
